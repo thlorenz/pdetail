7b3f99126562048ee6a8a9f5e7824bbf3b81e600
Thorsten Lorenz <thlorenz@gmx.de>
2017-09-07 06:47:35 +0000

    code: use strict

--- pdetail.js
+++ pdetail.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const ranks = [ 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' ]
 const suits = [ 'h', 'c', 'd', 's' ]
 
--- test/pdetail.js
+++ test/pdetail.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const test = require('tape')
 const { detailRange, detailRangeAll, rangeFromDetail } = require('../')
 
